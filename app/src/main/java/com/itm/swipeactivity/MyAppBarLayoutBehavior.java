00e8e8d2362bf7b9e17dd82713a444560b9bcbf1
alexey <sidorenko.al@gmail.com>
2016-06-16 10:57:17 +0300

    version 3

--- app/src/main/java/com/itm/swipeactivity/MyAppBarLayoutBehavior.java
+++ app/src/main/java/com/itm/swipeactivity/MyAppBarLayoutBehavior.java
@@ -1,12 +1,9 @@
 package com.itm.swipeactivity;
 
-import android.animation.Animator;
-import android.animation.AnimatorListenerAdapter;
-import android.animation.ValueAnimator;
 import android.app.Activity;
 import android.content.Context;
+import android.content.res.TypedArray;
 import android.graphics.Point;
-import android.support.annotation.Nullable;
 import android.support.design.widget.AppBarLayout;
 import android.support.design.widget.CoordinatorLayout;
 import android.support.v4.view.GestureDetectorCompat;
@@ -17,24 +14,21 @@ import android.view.GestureDetector;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.animation.LinearInterpolator;
 
 /**
  * Created by Alexey Sidorenko on 15-Jun-16.
  */
 public class MyAppBarLayoutBehavior extends AppBarLayout.Behavior implements
         GestureDetector.OnGestureListener {
-    private View mTargetView;
-
     private static final String TAG = MyAppBarLayoutBehavior.class.getSimpleName();
-    private int mStartY;
+    private View mTopView;
+    private View mBottomView;
     private int mNewHeight;
     private AppBarLayout.OnOffsetChangedListener mOffsetChangedListener;
     private int mCurrentOffset;
     private Context mContext;
-    private int mWidth;
-    private int mHeight;
     private GestureDetectorCompat mDetector;
+    private int mCloseMargin;
 
     public MyAppBarLayoutBehavior() {
         init();
@@ -43,10 +37,22 @@ public class MyAppBarLayoutBehavior extends AppBarLayout.Behavior implements
     public MyAppBarLayoutBehavior(Context context, AttributeSet attrs) {
         super(context, attrs);
         mContext = context;
+        setupAttrs(context, attrs);
         init();
     }
 
+    private void setupAttrs(Context context, AttributeSet attrs) {
+        TypedArray a = context.getTheme().obtainStyledAttributes(attrs,
+                R.styleable.MyAppBarLayoutBehavior, 0, 0);
+        try {
+            mCloseMargin = a.getDimensionPixelSize(R.styleable.MyAppBarLayoutBehavior_layout_close_margin, 0);
+        } finally {
+            a.recycle();
+        }
+    }
+
     private void init() {
+        // will save current offset for app bar layout
         mOffsetChangedListener = new AppBarLayout.OnOffsetChangedListener() {
             @Override
             public void onOffsetChanged(AppBarLayout appBarLayout, int verticalOffset) {
@@ -54,93 +60,77 @@ public class MyAppBarLayoutBehavior extends AppBarLayout.Behavior implements
             }
         };
 
-        Display display = ((Activity) mContext).getWindowManager().getDefaultDisplay();
-        Point size = new Point();
-        display.getSize(size);
-        mWidth = size.x;
-        mHeight = size.y;
-
+        // app bar doesn't trigger Behavior methods except onTouch....
+        // will use gesture detector for handling scrolling
         mDetector = new GestureDetectorCompat(mContext, this);
-
-    }
-
-    @Override
-    protected void layoutChild(CoordinatorLayout parent, AppBarLayout child, int layoutDirection) {
-        super.layoutChild(parent, child, layoutDirection);
-    }
-
-    @Override
-    public boolean onStartNestedScroll(CoordinatorLayout parent, AppBarLayout child, View directTargetChild, View target, int nestedScrollAxes) {
-        mStartY = 0;
-        Log.v(TAG, "onStartNestedScroll");
-        return super.onStartNestedScroll(parent, child, directTargetChild, target, nestedScrollAxes);
-    }
-
-    @Override
-    public void onNestedPreScroll(CoordinatorLayout coordinatorLayout, AppBarLayout child, View target, int dx, int dy, int[] consumed) {
-        super.onNestedPreScroll(coordinatorLayout, child, target, dx, dy, consumed);
-        Log.v(TAG, "onNestedPreScroll " + dy);
     }
 
     @Override
     public void onNestedScroll(CoordinatorLayout coordinatorLayout, AppBarLayout child, View target, int dxConsumed, int dyConsumed, int dxUnconsumed, int dyUnconsumed) {
         super.onNestedScroll(coordinatorLayout, child, target, dxConsumed, dyConsumed, dxUnconsumed, dyUnconsumed);
-        Log.v(TAG, "dyConsumed " + dyConsumed + " dyUnconsumed " + dyUnconsumed + " " + target.getClass().getSimpleName()
-                + " " + child.getClass().getSimpleName() + " mCurrentOffset " + mCurrentOffset);
+        checkScrollTopView(dyUnconsumed);
+        checkScrollBottomView(dyUnconsumed);
+    }
 
-        if (dyUnconsumed < 0 && mTargetView != null && mCurrentOffset == 0) {
-            ViewGroup.LayoutParams ll = mTargetView.getLayoutParams();
-            mNewHeight = ll.height - dyUnconsumed;
-            if (mNewHeight > 0 && mNewHeight <= 50) {
+    private boolean checkScrollBottomView(int distanceY) {
+        if (distanceY > 0 && mBottomView != null) {
+            ViewGroup.LayoutParams ll = mBottomView.getLayoutParams();
+            mNewHeight = ll.height + distanceY;
+            if (mNewHeight > 0 && mNewHeight <= mCloseMargin) {
                 Log.v(TAG, "newHeight " + mNewHeight);
                 ll.height = mNewHeight;
-                mTargetView.setLayoutParams(ll);
-            } else if (mNewHeight > 50 && mContext instanceof Activity) {
-                startExitAnimation();
+                mBottomView.setLayoutParams(ll);
+            } else if (mNewHeight > mCloseMargin && mContext instanceof Activity) {
+                slideOut(true);
             }
+            return true;
         }
+        return false;
     }
 
-    private void startExitAnimation() {
-        final ValueAnimator animator = ValueAnimator.ofInt(mNewHeight, mHeight / 2);
-        animator.setInterpolator(new LinearInterpolator());
-        animator.setDuration(300);
-        animator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
-            @Override
-            public void onAnimationUpdate(ValueAnimator animation) {
-                int val = (Integer) animation.getAnimatedValue();
-                ViewGroup.LayoutParams ll = mTargetView.getLayoutParams();
-                ll.height = val;
-                mTargetView.setLayoutParams(ll);
+    private boolean checkScrollTopView(int distanceY) {
+        if (distanceY < 0 && mTopView != null && mCurrentOffset == 0) {
+            ViewGroup.LayoutParams ll = mTopView.getLayoutParams();
+            mNewHeight = ll.height - distanceY;
+            if (mNewHeight > 0 && mNewHeight <= mCloseMargin) {
+                Log.v(TAG, "newHeight " + mNewHeight);
+                ll.height = mNewHeight;
+                mTopView.setLayoutParams(ll);
+            } else if (mNewHeight > mCloseMargin && mContext instanceof Activity) {
+                slideOut(false);
             }
-        });
-        animator.addListener(new AnimatorListenerAdapter() {
-            @Override
-            public void onAnimationEnd(Animator animation) {
-                super.onAnimationEnd(animation);
-                ((Activity) mContext).finish();
-            }
-        });
-        animator.start();
+            return true;
+        }
+        return false;
     }
-
 
     @Override
     public void onStopNestedScroll(CoordinatorLayout coordinatorLayout, AppBarLayout abl, View target) {
         super.onStopNestedScroll(coordinatorLayout, abl, target);
-        if (mTargetView != null && mNewHeight > 0) {
-            ViewGroup.LayoutParams ll = mTargetView.getLayoutParams();
+        hideBothViews();
+    }
+
+    private void hideBothViews() {
+        if (mNewHeight > 0) {
+            ViewGroup.LayoutParams ll = mTopView.getLayoutParams();
             ll.height = 0;
+            mTopView.setLayoutParams(ll);
+            ll = mBottomView.getLayoutParams();
+            ll.height = 0;
+            mBottomView.setLayoutParams(ll);
             mNewHeight = 0;
-            mTargetView.setLayoutParams(ll);
         }
     }
 
     @Override
     public boolean onLayoutChild(CoordinatorLayout parent, AppBarLayout child, int layoutDirection) {
         boolean superLayout = super.onLayoutChild(parent, child, layoutDirection);
-        if (mTargetView == null) {
-            mTargetView = parent.getRootView().findViewWithTag("testTag");
+        // find top and bottom views
+        if (mTopView == null) {
+            mTopView = parent.getRootView().findViewWithTag("topView");
+        }
+        if (mBottomView == null) {
+            mBottomView = parent.getRootView().findViewWithTag("bottomView");
         }
         return superLayout;
     }
@@ -152,31 +142,13 @@ public class MyAppBarLayoutBehavior extends AppBarLayout.Behavior implements
     }
 
     @Override
-    public boolean onDependentViewChanged(CoordinatorLayout parent, AppBarLayout child, View dependency) {
-        Log.v(TAG, "onDependentViewChanged");
-        return super.onDependentViewChanged(parent, child, dependency);
-    }
-
-    @Override
-    public boolean onInterceptTouchEvent(CoordinatorLayout parent, AppBarLayout child, MotionEvent ev) {
-//        Log.v(TAG, "onInterceptTouchEvent " + ev);
-        return super.onInterceptTouchEvent(parent, child, ev);
-    }
-
-    @Override
     public boolean onTouchEvent(CoordinatorLayout parent, AppBarLayout child, MotionEvent ev) {
-        if (ev.getAction() == MotionEvent.ACTION_UP){
-            if (mTargetView != null && mNewHeight > 0) {
-                ViewGroup.LayoutParams ll = mTargetView.getLayoutParams();
-                ll.height = 0;
-                mNewHeight = 0;
-                mTargetView.setLayoutParams(ll);
-            }
+        if (ev.getAction() == MotionEvent.ACTION_UP) {
+            hideBothViews();
+            return super.onTouchEvent(parent, child, ev);
         } else {
-            this.mDetector.onTouchEvent(ev);
+            return this.mDetector.onTouchEvent(ev) || super.onTouchEvent(parent, child, ev);
         }
-//        Log.v(TAG, "onTouchEvent " + ev);
-        return super.onTouchEvent(parent, child, ev);
     }
 
 
@@ -197,32 +169,30 @@ public class MyAppBarLayoutBehavior extends AppBarLayout.Behavior implements
 
     @Override
     public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY) {
-        Log.v(TAG, "scroll " + distanceY);
-
-        if (distanceY < 0 && mTargetView != null && mCurrentOffset == 0) {
-            ViewGroup.LayoutParams ll = mTargetView.getLayoutParams();
-            mNewHeight = ll.height - (int) distanceY;
-            if (mNewHeight > 0 && mNewHeight <= 50) {
-                Log.v(TAG, "newHeight " + mNewHeight);
-                ll.height = mNewHeight;
-                mTargetView.setLayoutParams(ll);
-            } else if (mNewHeight > 50 && mContext instanceof Activity) {
-                startExitAnimation();
-            }
-        }
-
-
-        return false;
+        return checkScrollTopView((int) distanceY);
     }
 
     @Override
     public void onLongPress(MotionEvent e) {
-
     }
 
     @Override
     public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
         return false;
     }
+
+    private void slideOut(boolean upAnimation) {
+        if (mContext instanceof Activity) {
+            Activity activity = ((Activity) mContext);
+            activity.finish();
+            if (upAnimation) {
+                activity.overridePendingTransition(R.anim.slide_down, R.anim.slide_up);
+            } else {
+                activity.overridePendingTransition(R.anim.slide_in_up, R.anim.slide_down);
+            }
+
+        }
+    }
+
 }
 
